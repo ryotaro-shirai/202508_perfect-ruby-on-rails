113e369f9abbff515ffaec0a4873d5213c1e8bb7
ryotaro-shirai <ryotaro.shirai@giftee.co>
2025-08-21 05:56:08 +0900

    バリデーションの追加

--- 02_book_admin/book_admin/app/models/book.rb
+++ 02_book_admin/book_admin/app/models/book.rb
@@ -5,4 +5,13 @@ class Book < ApplicationRecord
   belongs_to :publisher
   has_many :book_authors
   has_many :authors, through: :book_authors
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 25 }
+  validates :price, numericality: { greater_than_or_equal: 0 }
+  validate do |book|
+    if book.name.include?("exercise")
+      book.errors.add(:name, "I don't like exercise")
+    end
+  end
 end
