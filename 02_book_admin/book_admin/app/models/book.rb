d3cc58254cf197b9801b2dd161a59a62fb28f5f9
ryotaro-shirai <ryotaro.shirai@giftee.co>
2025-08-21 06:17:17 +0900

    特定の条件下のカスタムコールバックを追加

--- 02_book_admin/book_admin/app/models/book.rb
+++ 02_book_admin/book_admin/app/models/book.rb
@@ -9,9 +9,9 @@ class Book < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum: 25 }
   validates :price, numericality: { greater_than_or_equal: 0 }
-  validate do |book|
-    if book.name.include?("exercise")
-      book.errors.add(:name, "I don't like exercise")
+  validate do
+    if self.name.include?("exercise")
+      self.errors[:name] << "I don't like exercise"
     end
   end
 
@@ -21,9 +21,18 @@ class Book < ApplicationRecord
     Rails.logger.info "Book is deleted: #{self.attributes}"
   end
 
+  after_destroy :if => :high_price? do
+    Rails.logger.warn "Book with high price is deleted: #{self.attributes}"
+    Rails.logger.warn "Please Check!"
+  end
+
   def add_lovely_to_cat
     self.name = self.name.gsub(/Cat/) do |matched|
       "Lovely #{matched}"
     end
+  end
+
+  def high_price?
+    self.price >= 5000
   end
 end
