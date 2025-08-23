89e74a25024f266e8c5a6d11075f64f9ffc091ec
ryotaro-shirai <ryotaro.shirai@giftee.co>
2025-08-24 06:54:08 +0900

    開発者モードは modern browser として認識されないので、本番環境のみに allow_browser を適用

--- 02_book_admin/book_admin/app/controllers/application_controller.rb
+++ 02_book_admin/book_admin/app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # 開発者モードは modern browser として認識されないので、本番環境のみに適用
+  if Rails.env.production?
+    allow_browser versions: :modern
+  end
 end
