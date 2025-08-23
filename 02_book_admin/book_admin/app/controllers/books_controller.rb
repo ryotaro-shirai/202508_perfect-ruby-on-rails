953f42df813f48906cafe952a1208704d3b8da39
ryotaro-shirai <ryotaro.shirai@giftee.co>
2025-08-23 15:45:53 +0900

    rails g controller books

---
+++ 02_book_admin/book_admin/app/controllers/books_controller.rb
@@ -0,0 +1,24 @@
+class BooksController < ApplicationController
+  protect_from_forgery except: [:destroy]
+  before_action :set_book, only[:show, :destroy]
+
+  def show
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
+  def destroy
+    @book.destroy
+    respond_to do |format|
+      format.html { redirect_to "/"}
+      format.json { head :no_content}
+    end
+  end
+
+  private
+    def set_book
+      @book = Book.find(params[:id])
+    end
+end
