f645a2c14b8862dadd92889f8236133040c82a45
ryotaro-shirai <ryotaro.shirai@giftee.co>
2025-08-24 06:54:43 +0900

    mobile 版の view ファイルの追加

--- 02_book_admin/book_admin/app/controllers/application_controller.rb
+++ 02_book_admin/book_admin/app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   if Rails.env.production?
     allow_browser versions: :modern
   end
+
+  before_action :detect_mobile_variant
+
+  private
+
+    def detect_mobile_variant
+      request.variant = :mobile if request.user_agent =~ /iPhone/
+    end
 end
