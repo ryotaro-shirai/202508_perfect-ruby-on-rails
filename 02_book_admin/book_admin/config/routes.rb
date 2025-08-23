3d2ab6193b56c45f4246c5b8fdbe290857e47eb2
ryotaro-shirai <ryotaro.shirai@giftee.co>
2025-08-23 15:48:49 +0900

    rails g controller Profiles

--- 02_book_admin/book_admin/config/routes.rb
+++ 02_book_admin/book_admin/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "profiles/show"
+  get "profiles/edit"
+  get "profiles/update"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
